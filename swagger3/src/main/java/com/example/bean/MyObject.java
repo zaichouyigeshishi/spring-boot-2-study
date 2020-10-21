fe18c3e3a030def53de978a9ab44efacad4c4348
胡东山 <mj19910923@sina.com>
2020-10-21 19:21:55 +0800

    增加 swagger3 demo

--- swagger3/src/main/java/com/example/bean/MyObject.java
+++ swagger3/src/main/java/com/example/bean/MyObject.java
@@ -15,12 +15,12 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class MyObject {
 
-    @ApiModelProperty(value = "姓名")
+    @ApiModelProperty(value = "姓名",notes = "姓名不超过18个字符")
     private String name;
 
-    @ApiModelProperty(value = "手机号")
+    @ApiModelProperty(value = "手机号",notes = "手机号为11为纯数字")
     private String phone;
 
-    @ApiModelProperty(value = "年龄")
+    @ApiModelProperty(value = "年龄",notes = "年龄不超过200")
     private Integer age;
 }
